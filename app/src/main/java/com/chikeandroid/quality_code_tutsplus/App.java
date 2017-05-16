c17b952bb0f5e636ce89807380812eb06c8efed7
Chike <chikespott@gmail.com>
2017-05-16 17:37:45 +0100

    initial commit

---
+++ app/src/main/java/com/chikeandroid/quality_code_tutsplus/App.java
@@ -0,0 +1,30 @@
+package com.chikeandroid.quality_code_tutsplus;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+/**
+ * Created by Chike on 5/16/2017.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()   // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
+    }
+}
